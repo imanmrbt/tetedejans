0d486f4d1e304de279a9cc9a501a5fffc356858e
imanmrbt <128938797+imanmrbt@users.noreply.github.com>
2024-04-14 22:23:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ let handpos = [canvasBox.offsetWidth / 2, 70]
 
 let playing = true
 
-circle.restitution = 0.4;
+heads.restitution = 0.4;
 
 
 function preload() {
@@ -182,7 +182,7 @@ function checkCollisions(circles) {
             if (circleA.level === circleB.level && circleA.level < headsdata.length - 1 && Query.collides(circleA.body, [circleB.body]).length > 0) {
                 // Circles with the same type are touching each other
                 console.log(`Circles ${i} and ${j} with type ${circleA.level} are touching.`);
-                console.log(circleB);
+                
                 // if two objects of the same level group are touching
 
                 // create a a new object with one level higher level in the position between two previous bodies
